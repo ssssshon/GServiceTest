f3863cfa5eb511ca6e3a67c0fa0e945ba82f5e8d
hyeonji.shon <hyeonji.shon@samsung.com>
2017-04-24 16:51:55 +0900

    modifying LocationSetiings

--- app/src/main/java/example/shj/gservicetest/GoogleMapsActivity.java
+++ app/src/main/java/example/shj/gservicetest/GoogleMapsActivity.java
@@ -1,6 +1,9 @@
 package example.shj.gservicetest;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -17,12 +20,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient; //
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices; //
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
 import com.google.android.gms.location.places.Places; //
@@ -37,10 +48,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class GoogleMapsActivity extends AppCompatActivity
         implements OnMapReadyCallback,
-                GoogleApiClient.ConnectionCallbacks,
-                GoogleApiClient.OnConnectionFailedListener{
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener,
+        ResultCallback<LocationSettingsResult> {
 
-    private  static final String TAG = "GST:GoogleMap";
+    private static final String TAG = "GST:GoogleMap";
 
     private GoogleMap mMap;
     private CameraPosition mCameraPosition;
@@ -56,6 +70,13 @@ public class GoogleMapsActivity extends AppCompatActivity
     private boolean mLocationPermissionGranted;
 
     private Location mLastKnownLocation;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+
+    /**
+     * Constant used in the location settings dialog.
+     */
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     // Keys for storing activity state.
     private static final String KEY_CAMERA_POSITION = "camera_position";
@@ -72,7 +93,7 @@ public class GoogleMapsActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             mLastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
             mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
         }
@@ -91,6 +112,12 @@ public class GoogleMapsActivity extends AppCompatActivity
                 .build();
 
 
+        //createLocationRequest();
+        createLocationRequest();
+
+        //init for location settings
+        buildLocationSettingsRequest();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
         //        .findFragmentById(R.id.map);
@@ -102,7 +129,7 @@ public class GoogleMapsActivity extends AppCompatActivity
      */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        if(mMap != null) {
+        if (mMap != null) {
             outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
             outState.putParcelable(KEY_LOCATION, mLastKnownLocation);
             super.onSaveInstanceState(outState);
@@ -122,7 +149,8 @@ public class GoogleMapsActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         Log.i(TAG, "OnMapReady");
         mMap = googleMap;
-
+        mMap.setOnMyLocationButtonClickListener(this);
+        //mMap.getUiSettings().setMyLocationButtonEnabled(true); //애 때메 buttonclick listener 를 못바음
         //InfoWindow??? 이건 뭥?
         mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
             @Override
@@ -135,7 +163,7 @@ public class GoogleMapsActivity extends AppCompatActivity
             public View getInfoContents(Marker marker) {
                 // Inflate the layouts for the info window, title and snippet.
                 View infoWindow = getLayoutInflater().inflate(R.layout.custom_info_contents,
-                        (FrameLayout)findViewById(R.id.map), false);
+                        (FrameLayout) findViewById(R.id.map), false);
 
                 TextView title = ((TextView) infoWindow.findViewById(R.id.title));
                 title.setText(marker.getTitle());
@@ -157,6 +185,7 @@ public class GoogleMapsActivity extends AppCompatActivity
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 
     /**
      * Updates the map's UI settings based on whether the user has granted location permission.
@@ -184,10 +213,10 @@ public class GoogleMapsActivity extends AppCompatActivity
 
         if (mLocationPermissionGranted) {
             mMap.setMyLocationEnabled(true); //It needs to be granted 'Location permissions'
-            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            //mMap.getUiSettings().setMyLocationButtonEnabled(true);
         } else {
             mMap.setMyLocationEnabled(false);
-            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            //mMap.getUiSettings().setMyLocationButtonEnabled(false);
             mLastKnownLocation = null;
         }
     }
@@ -221,6 +250,10 @@ public class GoogleMapsActivity extends AppCompatActivity
             mLastKnownLocation = LocationServices.FusedLocationApi
                     .getLastLocation(mGoogleApiClient);
         }
+        //LastKnownLocation is none, try to get current location settings
+        if (mLastKnownLocation == null) {
+            checkLocationSettings();
+        }
 
         // Set the map's camera position to the current location of the device.
         if (mCameraPosition != null) {
@@ -232,9 +265,115 @@ public class GoogleMapsActivity extends AppCompatActivity
         } else {
             Log.d(TAG, "Current location is null. Using defaults.");
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
-            mMap.getUiSettings().setMyLocationButtonEnabled(false); //'set location button' is visible
+            //mMap.getUiSettings().setMyLocationButtonEnabled(false); //'set location button' is visible
         }
     }
+
+    /**
+     * Sets up the location request. Android has two location request settings:
+     * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
+     * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+     * the AndroidManifest.xml.
+     * <p/>
+     * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+     * interval (5 seconds), the Fused Location Provider API returns location updates that are
+     * accurate to within a few feet.
+     * <p/>
+     * These settings are appropriate for mapping applications that show real-time location
+     * updates.
+     */
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(10000);
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * Uses a {@link com.google.android.gms.location.LocationSettingsRequest.Builder} to build
+     * a {@link com.google.android.gms.location.LocationSettingsRequest} that is used for checking
+     * if a device has the needed location settings.
+     */
+    protected void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    /**
+     * Check if the device's location settings are adequate for the app's needs using the
+     * {@link com.google.android.gms.location.SettingsApi#checkLocationSettings(GoogleApiClient,
+     * LocationSettingsRequest)} method, with the results provided through a {@code PendingResult}.
+     */
+    protected void checkLocationSettings() {
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(
+                        mGoogleApiClient,
+                        mLocationSettingsRequest
+                );
+        result.setResultCallback(this);
+    }
+
+    /**
+     * The callback invoked when
+     * {@link com.google.android.gms.location.SettingsApi#checkLocationSettings(GoogleApiClient,
+     * LocationSettingsRequest)} is called. Examines the
+     * {@link com.google.android.gms.location.LocationSettingsResult} object and determines if
+     * location settings are adequate. If they are not, begins the process of presenting a location
+     * settings dialog to the user.
+     */
+    @Override
+    public void onResult(LocationSettingsResult locationSettingsResult) {
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                Log.i(TAG, "All location settings are satisfied.");
+                //startLocationUpdates();
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to" +
+                        "upgrade location settings ");
+
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result
+                    // in onActivityResult().
+                    status.startResolutionForResult(GoogleMapsActivity.this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.i(TAG, "PendingIntent unable to execute request.");
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog " +
+                        "not created.");
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+                    case REQUEST_CHECK_SETTINGS:
+                        switch (resultCode) {
+                            case Activity.RESULT_OK:
+                                Log.i(TAG, "User agreed to make required location settings changes.");
+                                startLocationUpdates();
+                                break;
+                            case Activity.RESULT_CANCELED:
+                                Log.i(TAG, "User chose not to make required location settings changes.");
+                                break;
+                }
+                break;
+        }
+    }
+
 
     /**
      * Handles the result of the request for location permissions.
@@ -278,6 +417,8 @@ public class GoogleMapsActivity extends AppCompatActivity
             result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
                 @Override
                 public void onResult(@NonNull PlaceLikelihoodBuffer likelyPlaces) {
+                    Log.v(TAG, "onReult!");
+
                     int i = 0;
                     mLikelyPlaceNames = new String[mMaxEntries];
                     mLikelyPlaceAddresses = new String[mMaxEntries];
@@ -302,6 +443,7 @@ public class GoogleMapsActivity extends AppCompatActivity
                     // Show a dialog offering the user the list of likely places, and add a
                     // marker at the selected place.
                     openPlacesDialog();
+                    Log.v(TAG, "openPlaceDialog!!!");
                 }
             });
         } else {
@@ -311,7 +453,7 @@ public class GoogleMapsActivity extends AppCompatActivity
                     .title("Marker in Sydney")
                     .position(mDefaultLocation)
                     .snippet("Here Sydney!"));
-          //  mMap.moveCamera(CameraUpdateFactory.newLatLng(mDefaultLocation));
+            //  mMap.moveCamera(CameraUpdateFactory.newLatLng(mDefaultLocation));
         }
     }
 
@@ -344,10 +486,10 @@ public class GoogleMapsActivity extends AppCompatActivity
                 };
 
         // Display the dialog.
-        AlertDialog dialog = new AlertDialog.Builder(this)
-                .setTitle(R.string.pick_place)
-                .setItems(mLikelyPlaceNames, listener)
-                .show();
+        //AlertDialog dialog = new AlertDialog.Builder(this)
+        //        .setTitle(R.string.pick_place)
+        //        .setItems(mLikelyPlaceNames, listener)
+        //        .show();
     }
 
 
@@ -405,4 +547,40 @@ public class GoogleMapsActivity extends AppCompatActivity
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient,
+                mLocationRequest,
+                this
+        ).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                //mRequestingLocationUpdates = true;
+                //setButtonsEnabledState();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Log.i(TAG, "MyLocation Buttons click!!!");
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        return false;
+    }
 }
